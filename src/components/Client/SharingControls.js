0c7dcecd97d02cbeac173922336b7c4c54de73c0
Wilkor <wilkorguitarrista@msn.com>
2025-01-08 12:09:15 -0300

    ajustando para aparacer somente audio quando estiver no celular

--- src/components/Client/SharingControls.js
+++ src/components/Client/SharingControls.js
@@ -1,7 +1,64 @@
-import React from 'react';
+// import React from 'react';
+// import { Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+
+// const SharingControls = ({ sharingOption, setSharingOption, audioDevices, selectedAudioDevice, setSelectedAudioDevice, startSharing }) => {
+//     return (
+//         <>
+//             <FormControl variant="outlined" fullWidth style={{ marginTop: '20px' }}>
+//                 <InputLabel>O que deseja compartilhar?</InputLabel>
+//                 <Select
+//                     value={sharingOption}
+//                     onChange={(e) => setSharingOption(e.target.value)}
+//                     label="O que deseja compartilhar?"
+//                 >
+//                     <MenuItem value="screen">Tela</MenuItem>
+//                     <MenuItem value="screenAudio">Tela e Áudio</MenuItem>
+//                     <MenuItem value="videoAudio">Vídeo e Áudio</MenuItem>
+//                     <MenuItem value="audio">Áudio</MenuItem>
+//                 </Select>
+//             </FormControl>
+//             {(sharingOption === 'audio' || sharingOption === 'videoAudio') && (
+//                 <FormControl variant="outlined" fullWidth style={{ marginTop: '20px' }}>
+//                     <InputLabel>Selecione o Dispositivo de Áudio</InputLabel>
+//                     <Select
+//                         value={selectedAudioDevice}
+//                         onChange={(e) => setSelectedAudioDevice(e.target.value)}
+//                         label="Selecione o Dispositivo de Áudio"
+//                     >
+//                         {audioDevices.map(device => (
+//                             <MenuItem key={device.deviceId} value={device.deviceId}>
+//                                 {device.label || `Dispositivo de áudio ${device.deviceId}`}
+//                             </MenuItem>
+//                         ))}
+//                     </Select>
+//                 </FormControl>
+//             )}
+//             <Button
+//                 fullWidth
+//                 variant="contained"
+//                 color="primary"
+//                 onClick={startSharing}
+//                 style={{ marginTop: '20px' }}
+//             >
+//                 Compartilhar
+//             </Button>
+//         </>
+//     );
+// };
+
+// export default SharingControls;
+import React, { useEffect, useState } from 'react';
 import { Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 const SharingControls = ({ sharingOption, setSharingOption, audioDevices, selectedAudioDevice, setSelectedAudioDevice, startSharing }) => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Detecta se o dispositivo é móvel usando o userAgent
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        setIsMobile(/android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase()));
+    }, []);
+
     return (
         <>
             <FormControl variant="outlined" fullWidth style={{ marginTop: '20px' }}>
@@ -11,10 +68,18 @@ const SharingControls = ({ sharingOption, setSharingOption, audioDevices, select
                     onChange={(e) => setSharingOption(e.target.value)}
                     label="O que deseja compartilhar?"
                 >
-                    <MenuItem value="screen">Tela</MenuItem>
-                    <MenuItem value="screenAudio">Tela e Áudio</MenuItem>
-                    <MenuItem value="videoAudio">Vídeo e Áudio</MenuItem>
-                    <MenuItem value="audio">Áudio</MenuItem>
+                    {isMobile ? (
+                        // Apenas opções de áudio para dispositivos móveis
+                        <MenuItem value="audio">Áudio</MenuItem>
+                    ) : (
+                        // Todas as opções para desktops
+                        <>
+                            <MenuItem value="screen">Tela</MenuItem>
+                            <MenuItem value="screenAudio">Tela e Áudio</MenuItem>
+                            <MenuItem value="videoAudio">Vídeo e Áudio</MenuItem>
+                            <MenuItem value="audio">Áudio</MenuItem>
+                        </>
+                    )}
                 </Select>
             </FormControl>
             {(sharingOption === 'audio' || sharingOption === 'videoAudio') && (
